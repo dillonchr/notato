ec6e22bcba026f1d79effd24e62d910c1aefbb20
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-12-15 23:06:28 -0600

    omg finally got dms working thanks to https://stackoverflow.com/questions/69275566/discord-bot-not-detecting-messages#answer-69275630

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 const { Sequelize, Model, DataTypes } = require("sequelize");
+
+// Create a new client instance
+const client = new Client({
+  intents: [Intents.FLAGS.DIRECT_MESSAGES],
+  partials: ["MESSAGE", "CHANNEL"]
+});
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageCreate", message => {
+  console.log("GOTTQ MASDMMGA");
+  if (message.author.username !== client.user.username) {
+    console.log({ message });
+  }
+  console.log({ message });
+});
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
+  }
+});
+
+// Login to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
+
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "/Users/dillon/git/DATAS/notato.sqlite"
@@ -14,6 +52,7 @@ Note.init(
   { sequelize, modelName: "note" }
 );
 
+/*
 (async () => {
   await sequelize.sync();
   try {
@@ -27,3 +66,4 @@ Note.init(
     console.log({ err });
   }
 })();
+*/
